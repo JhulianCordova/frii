4bd59a803bb0ec6f63398d6e0b37357e7a75275e
Elvis Aldair <aldaelvis@hotmail.com>
2020-04-26 20:03:18 -0500

    .

--- app/src/main/java/com/cor/frii/Cart.java
+++ app/src/main/java/com/cor/frii/Cart.java
@@ -25,34 +25,35 @@ public class Cart extends AppCompatActivity implements NavigationView.OnNavigati
         GasFragment.OnFragmentInteractionListener,
         MainFragment.OnFragmentInteractionListener,
         ProductsFragment.OnFragmentInteractionListener,
-        SettingFragment.OnFragmentInteractionListener{
+        SettingFragment.OnFragmentInteractionListener {
 
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_cart);
         super.onCreate(savedInstanceState);
 
-        toolbar=findViewById(R.id.navigationToolbar);
+        toolbar = findViewById(R.id.navigationToolbar);
         setSupportActionBar(toolbar);
-        drawerLayout=findViewById(R.id.navigationDrawerCart);
-        navigationView=findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.navigationDrawerCart);
+        navigationView = findViewById(R.id.navigationView);
 
         // estaclecer el evento onclick de navigation
         navigationView.setNavigationItemSelectedListener(this);
 
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
 
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.navigationContainer,new CartdetailFragment());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.navigationContainer, new CartdetailFragment());
         fragmentTransaction.commit();
 
         /*
@@ -69,34 +70,36 @@ public class Cart extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         drawerLayout.closeDrawer(GravityCompat.START);
-        FragmentManager manager=getSupportFragmentManager();
-        FragmentTransaction transaction=manager.beginTransaction();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
         Intent intent;
-        if (menuItem.getItemId()==R.id.home){
+        if (menuItem.getItemId()==R.id.home) {
 
-             intent=new Intent(this,MainActivity.class);
+            intent = new Intent(this, MainActivity.class);
             //transaction.replace(R.id.navigationContainer,new MainFragment());
             startActivity(intent);
             finish();
             //transaction.commit();
-            Toast.makeText(this,"ingreso a home",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "ingreso a home", Toast.LENGTH_SHORT).show();
         }
-        if (menuItem.getItemId()==R.id.account){
-             intent=new Intent(getBaseContext(),PerfilActivity.class);
-            intent.putExtra("id",R.id.account);
+        if (menuItem.getItemId() == R.id.account) {
+            intent = new Intent(getBaseContext(), PerfilActivity.class);
+            intent.putExtra("id", R.id.account);
             startActivity(intent);
-            Toast.makeText(this,"ingreso a account",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "ingreso a account", Toast.LENGTH_SHORT).show();
         }
-        if(menuItem.getItemId()==R.id.Perfil){
-            transaction.replace(R.id.navigationContainer,new SettingFragment());
+        if (menuItem.getItemId() == R.id.Perfil) {
+            transaction.replace(R.id.navigationContainer, new SettingFragment());
             transaction.addToBackStack(null);
             transaction.commit();
-            Toast.makeText(this,"ingreso a configuracion",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "ingreso a configuracion", Toast.LENGTH_SHORT).show();
         }
-        if (menuItem.getItemId()==R.id.MisPedidos){
-            intent=new Intent(this,PedidosActivity.class);
+        if (menuItem.getItemId() == R.id.MisPedidos) {
+            intent = new Intent(this, PedidosActivity.class);
             startActivity(intent);
         }
         return false;
     }
+
+
 }
