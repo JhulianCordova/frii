f24bc2e90a248d62eae6b47035ae83132df2e902
cordova <julian.cordova.sullca@gmail.com>
2020-04-28 13:52:52 -0500

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	.idea/gradle.xml
    #	build.gradle

--- app/src/main/java/com/cor/frii/AccountFragment.java
+++ app/src/main/java/com/cor/frii/AccountFragment.java
@@ -129,6 +129,8 @@ public class AccountFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
