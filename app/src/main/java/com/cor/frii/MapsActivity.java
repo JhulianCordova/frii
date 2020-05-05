d9b0a660dddaec525742844c6be79bd0eb2ac94e
Elvis Aldair <aldaelvis@hotmail.com>
2020-05-05 17:19:39 -0500

    Agregando mapas

--- app/src/main/java/com/cor/frii/MapsActivity.java
+++ app/src/main/java/com/cor/frii/MapsActivity.java
@@ -4,10 +4,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,9 +36,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
     }
 }
--- app/src/main/java/com/cor/frii/ProcesarpedidoFragment.java
+++ app/src/main/java/com/cor/frii/ProcesarpedidoFragment.java
@@ -2,15 +2,39 @@ package com.cor.frii;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.cor.frii.utils.GpsUtils;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 /**
@@ -21,7 +45,7 @@ import android.widget.Button;
  * Use the {@link ProcesarpedidoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProcesarpedidoFragment extends Fragment {
+public class ProcesarpedidoFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,6 +56,22 @@ public class ProcesarpedidoFragment extends Fragment {
     private String mParam2;
 
     private Button procesarPedido;
+    private static final String TAG = "GAS";
+    private static final int DEFAULT_ZOOM = 16;
+    private Location mLastKnownLocation;
+    private GoogleMap map;
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private Marker marcador;
+    private Marker marcador_carro;
+    private Address address;
+
+
+    private ArrayList<LatLng> mMarkerPoints;
+
+    TextView lblDireccion;
 
     private OnFragmentInteractionListener mListener;
 
@@ -39,15 +79,7 @@ public class ProcesarpedidoFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProcesarpedidoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ProcesarpedidoFragment newInstance(String param1, String param2) {
         ProcesarpedidoFragment fragment = new ProcesarpedidoFragment();
         Bundle args = new Bundle();
@@ -70,19 +102,26 @@ public class ProcesarpedidoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_procesarpedido, container, false);
+        View view = inflater.inflate(R.layout.fragment_procesarpedido, container, false);
+        lblDireccion = view.findViewById(R.id.lblDireccion);
 
-        procesarPedido=view.findViewById(R.id.ButtonConfirmarPedido);
-        procesarPedido.setOnClickListener(new View.OnClickListener(){
+        procesarPedido = view.findViewById(R.id.ButtonConfirmarPedido);
+        procesarPedido.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getContext(),PedidosActivity.class);
+                Intent intent = new Intent(getContext(), PedidosActivity.class);
                 startActivity(intent);
             }
         });
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.google_map_pedidos);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
 
+        mMarkerPoints = new ArrayList<>();
 
         return view;
     }
@@ -111,16 +150,171 @@ public class ProcesarpedidoFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        map.setOnMarkerClickListener(this);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
+        getLocationPermission();
+        updateLocationUI();
+        getDeviceLocation();
+
+        System.out.println(mMarkerPoints);
+        System.out.println(mMarkerPoints.size());
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                if (mMarkerPoints.size() > 0) {
+                    mMarkerPoints.clear();
+                    map.clear();
+                }
+
+                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(point.latitude, point.longitude, 1);
+
+                    if (addresses.size() > 0) {
+                        address = addresses.get(0);
+                        lblDireccion.setText(address.getAddressLine(0));
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mMarkerPoints.add(point);
+                map.addMarker(new MarkerOptions()
+                        .position(point)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                System.out.println(mMarkerPoints);
+                System.out.println(mMarkerPoints.size());
+            }
+        });
+
+
+    }
+
+
+    private void getDeviceLocation() {
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            mLastKnownLocation = task.getResult();
+                            if (mLastKnownLocation != null) {
+                                if (marcador != null) marcador.remove();
+
+                                LatLng latLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+                                mMarkerPoints.add(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()));
+                                map.addMarker(new MarkerOptions().position(mMarkerPoints.get(0))
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+                                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+                                try {
+                                    List<Address> addresses = geocoder.getFromLocation(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude(), 1);
+
+                                    if (addresses.size() > 0) {
+                                        address = addresses.get(0);
+                                        lblDireccion.setText(address.getAddressLine(0));
+                                    }
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            } else {
+                                new GpsUtils(getActivity()).turnGPSOn(new GpsUtils.onGpsListener() {
+                                    @Override
+                                    public void gpsStatus(boolean isGPSEnable) {
+                                        if (isGPSEnable) {
+                                            //---------------------------------
+                                            updateLocationUI();
+                                            getDeviceLocation();
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (map == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+
+            } else {
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
